ca6458d2a5e36fa5c1f03e19471cca4bdf8f8e8b
TonyPadilha <tonypadilha2023@gmail.com>
2025-03-11 11:36:32 -0400

    NOVAS ALTERAÇÕES.

--- static/app.js
+++ static/app.js
@@ -5,7 +5,7 @@ $('form input[type="file"]').change(event => {
   } else {
       if(arquivos[0].type == 'image/jpeg') {
         $('img').remove();
-        let imagem = $('<img class="img-fluid">');
+        let imagem = $('<img class="img-responsive">');
         imagem.attr('src', window.URL.createObjectURL(arquivos[0]));
         $('figure').prepend(imagem);
       } else {
